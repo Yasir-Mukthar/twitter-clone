2516d14ca657efd882856d62316a6fabcf83ead9
Yasir-Mukthar <yasirmukhtartar@example.com>
2024-06-20 23:56:43 -0700

    added notification controllers

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -1,10 +1,37 @@
 
 
+import Notification from "../models/notification.model.js";
 
+const getNotifications=async(req,res)=>{
+    try {
+		const userId = req.user._id
+        console.log(userId);
 
-const getNotifications=async(req,res)=>{}
+		const notifications = await Notification.find({ to: userId }).populate({
+			path: "from",
+			select: "username profileImg",
+		});
+        console.log("notifications all");
 
-const deleteNotifications=async(req,res)=>{}
+		await Notification.updateMany({ to: userId }, { read: true });
+
+		res.status(200).json(notifications);
+	} catch (error) {
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+}
+
+const deleteNotifications=async(req,res)=>{
+    try {
+		const userId = req.user._id;
+
+		await Notification.deleteMany({ to: userId });
+
+		res.status(200).json({ message: "Notifications deleted successfully" });
+	} catch (error) {
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+}
 
 
 
